70232782a97f29eb27dc7a1612de42f5760a5edd
Glenn Hoeppner <ghoeppner@imac.attlocal.net>
2014-10-02 11:17:22 -0400

    add publish script

---
+++ Tools/publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# This script simply publishes CleanWeb locally for development testing
+# It mearly is a shortcut for copying files to what I have configured
+# as my user's wwwroot, and makes a lot of assumptions about how the
+# environment is configured.
+#
+echo "Publishing..."
+cp -r ~/Projects/CleanWeb/Source/ ~/Sites
+echo "Done."
+echo "Setting permissions..."
+chmod -R +rx ~/Sites
+echo "Done."
