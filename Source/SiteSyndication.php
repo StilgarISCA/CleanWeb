8b1030921ab076b77d3dd0b4b21bf6aa68a00617
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2014-10-05 10:48:39 -0400

    Make SiteSyndication getSiteFeeds() method duplicate existing functionality.

--- Source/SiteSyndication.php
+++ Source/SiteSyndication.php
@@ -31,22 +31,11 @@ class SiteSyndication
    {
       $elements = $this->_xPath->query( "/html/head/link[@type='application/rss+xml']" );
       
-      if ( $elements->length > 0 )
-         foreach ( $elements as $feedUrl ) {
-            print 'Found ' . $feedUrl->getAttribute( 'href' ) .'<br>';
-         }
-         
-         /*
       if( $elements->length > 0
           && ( $elements->item(0)->getAttribute( 'href' ) != ''
                || $elements->item(0)->getAttribute( 'href' ) == null )
       ) 
          return $elements->item(0)->getAttribute( 'href' ); 
-         */
    } // end getSiteFeeds()
 } // end SiteSyndication()
-
-$foo = new SiteSyndication( "www.sltrib.com" );
-$foo->getSiteFeeds();
-
 ?>
