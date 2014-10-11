885b7856d8ca8f180dd6b190ebdf46af83c1f7e4
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2014-10-11 11:41:15 -0400

    Util function working

---
+++ IntegrationTests/UrlUtilsTests.php
@@ -0,0 +1,9 @@
+<?php
+require_once( './UrlUtils.inc' );
+
+$urlToEncode = 'http://www.php.net/';
+$encodedUrl = 'aHR0cCUzQSUyRiUyRnd3dy5waHAubmV0JTJG';
+
+print "Url: $urlToEncode as encoded is: " . UrlUtils::CleanWebEncode( $urlToEncode ) . '<br>';
+print "Encoded string: $encodedUrl decoded is: " . UrlUtils::CleanWebDecode( $encodedUrl ) . '<br>';
+?>
